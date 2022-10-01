57c9cf20691530eaa332e915002406296c72084e
danilbutakov <12021109359@irgups.ru>
2022-10-01 17:46:50 +0800

    Сделал сохранение параметров и фильтраций в URL

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import axios from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
+import qs from 'qs';
+import { useNavigate } from 'react-router-dom';
 
-import { setCategoryId, setCurrentPage } from '../redux/slices/filterSlice';
+import { setCategoryId, setCurrentPage, setFilters } from '../redux/slices/filterSlice';
+import { sortList } from '../components/Sort/Sort';
 import Categories from '../components/Categories'
 import PizzaBlock from '../components/PizzaBlock/Index'
 import Sort from '../components/Sort/Sort'
@@ -12,39 +15,83 @@ import Pagination from '../components/Pagination/Index';
 
 const Home = () => {
 
+    const navigate = useNavigate();
     const dispatch = useDispatch();
+    const isSearch = React.useRef(false);
+    const isMounted = React.useRef(false);
+
     const { categoryId, sort, currentPage } = useSelector((state) => state.filter);
 
+    const { searchValue } = React.useContext(AppContext);
     const [items, setItems] = React.useState([]);
     const [isLoading, setIsLoading] = React.useState(true);
 
-    const onChangeCategory = (id) => {
-        dispatch(setCategoryId(id));
-    }
+    const onChangeCategory = React.useCallback((idx) => {
+        dispatch(setCategoryId(idx));
+    }, []);
 
-    const { searchValue } = React.useContext(AppContext);
+    const onChangePage = (page) => {
+        dispatch(setCurrentPage(page));
+    };
 
+    const fetchPizzas = () => {
+        setIsLoading(true);
+
+        const sortBy = sort.sortProperty.replace('-', '');
+        const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
+        const category = categoryId > 0 ? `category=${categoryId}` : '';
+        const search = searchValue ? `search=${searchValue}` : '';
+
+        axios
+        .get(
+            `https://63343f9f433198e79dd3b5ea.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}&${search}`,
+        )
+        .then((res) => {
+            setItems(res.data);
+            setIsLoading(false);
+        });
+    };
+
+    // Если изменили параметры и был первый рендер
     React.useEffect(() => {
-        async function fetchData() {
-            try {
-                setIsLoading(true);
+        if (isMounted.current) {
+        const queryString = qs.stringify({
+            sortProperty: sort.sortProperty,
+            categoryId,
+            currentPage,
+        });
 
-                const sortBy = sort.sortProperty.replace('-', '');
-                const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
-                const category = categoryId > 0 ? `category=${categoryId}` : '';
-                const search = searchValue ? `&search=${searchValue}` : '';
-
-                const itemsResponse = await axios.get(`https://63343f9f433198e79dd3b5ea.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`);
-
-                setIsLoading(false);
-                setItems(itemsResponse.data);
-            } catch (error) {
-                alert('Ошибка при запросе данных ;(');
-                console.error(error);
-            }
+        navigate(`?${queryString}`);
         }
-        fetchData();
+        isMounted.current = true;
+    }, [categoryId, sort.sortProperty, currentPage]);
+
+    // Если был первый рендер, то проверяем URL-параметры и сохраняем в редуксе
+    React.useEffect(() => {
+        if (window.location.search) {
+        const params = qs.parse(window.location.search.substring(1));
+
+        const sort = sortList.find((obj) => obj.sortProperty === params.sortProperty);
+
+        dispatch(
+            setFilters({
+            ...params,
+            sort,
+            }),
+        );
+        isSearch.current = true;
+        }
+    }, []);
+
+    // Если был первый рендер, то запрашиваем пиццы
+    React.useEffect(() => {
         window.scrollTo(0, 0);
+
+        if (!isSearch.current) {
+        fetchPizzas();
+        }
+
+        isSearch.current = false;
     }, [categoryId, sort.sortProperty, searchValue, currentPage]);
 
     const pizzas = items.map((obj) => (
@@ -54,10 +101,6 @@ const Home = () => {
         />));
 
     const skeleton = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
-
-    const onChangePage = (number) => {
-        dispatch(setCurrentPage(number));
-    }
 
     return (
         <div className='container'>
