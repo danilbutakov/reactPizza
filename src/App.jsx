8203a8449064fd8eb07ce8e12f541789154bacd8
danilbutakov <12021109359@irgups.ru>
2022-10-09 17:49:52 +0800

    Убрал отображение cart в /cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import React from "react";
-import AppContext from "./context";
+import React from 'react';
+import AppContext from './context';
 
-import Header from "./components/Header";
-import "./scss/app.scss";
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Empty from "./pages/Empty";
-import Cart from "./pages/Cart";
+import Header from './components/Header';
+import './scss/app.scss';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Empty from './pages/Empty';
+import Cart from './pages/Cart';
 
 function App() {
-  const [searchValue, setSearchValue] = React.useState("");
+	const [searchValue, setSearchValue] = React.useState('');
 
-  return (
-    <div className="wrapper">
-      <AppContext.Provider value={{ searchValue, setSearchValue }}>
-        <Header />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="*" element={<Empty />} />
-          </Routes>
-        </div>
-      </AppContext.Provider>
-    </div>
-  );
+	return (
+		<div className='wrapper'>
+			<AppContext.Provider value={{ searchValue, setSearchValue }}>
+				<Header />
+				<div className='content'>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/cart' element={<Cart />} />
+						<Route path='*' element={<Empty />} />
+					</Routes>
+				</div>
+			</AppContext.Provider>
+		</div>
+	);
 }
 
 export default App;
