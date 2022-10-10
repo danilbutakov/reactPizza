d6e0305ee67e68125d32fad36a218f75fd8400dd
danilbutakov <12021109359@irgups.ru>
2022-10-10 22:56:08 +0800

    добавил android и ios в проект с помощью библиотеки capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,14 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'react-pizza',
+  webDir: 'build',
+  bundledWebRuntime: false,
+  server: {
+   url: 'https://react-pizza-beryl.vercel.app/',
+   cleartext: true
+  }
+};
+
+export default config;
